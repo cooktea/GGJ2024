bce88f7d364d0a0d930ef80d1662de3013e532f0
YPZhou <yunpeng_zhou@163.com>
2024-01-27 22:53:32 +0800

    Ball speed decrease by time

--- Game/Assets/Scripts/Implements/Ball.cs
+++ Game/Assets/Scripts/Implements/Ball.cs
@@ -5,174 +5,184 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour, IBall
 {
-    [SerializeField] GameObject Owner;
-    [SerializeField] BallState state;
-    IPlayer ownerPlayer => Owner?.GetComponent<IPlayer>();
-    Rigidbody2D rb;
+	[SerializeField] GameObject Owner;
+	[SerializeField] BallState state;
+	IPlayer ownerPlayer => Owner?.GetComponent<IPlayer>();
+	Rigidbody2D rb;
 
-    [Header("Movement")]
-    [SerializeField] float speed;
-    [SerializeField] Vector3 dir;
-    [SerializeField] float noContactTime = 0.5f;
-    [SerializeField] Vector2 nextWayPoint;
-    Queue<Vector2> wayPoints = new Queue<Vector2>();
+	[Header("Movement")]
+	[SerializeField] float speed;
+	[SerializeField] Vector3 dir;
+	[SerializeField] float noContactTime = 0.5f;
+	[SerializeField] Vector2 nextWayPoint;
+	Queue<Vector2> wayPoints = new Queue<Vector2>();
 
-    [Header("Collision Info")]
-    [SerializeField] ContactFilter2D contactFilter;
-    Collider2D[] others = new Collider2D[10];
-    Collider2D ballCollider;
-    enum BallState
-    {
-        Free,
-        Held,
-    }
+	[Header("Collision Info")]
+	[SerializeField] ContactFilter2D contactFilter;
+	Collider2D[] others = new Collider2D[10];
+	Collider2D ballCollider;
+	enum BallState
+	{
+		Free,
+		Held,
+	}
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        ballCollider = GetComponent<CircleCollider2D>();
-        wayPoints.Clear();
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		ballCollider = GetComponent<CircleCollider2D>();
+		wayPoints.Clear();
+	}
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        DoMove(Time.deltaTime);
-    }
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		DoMove(Time.deltaTime);
 
-    private void FreeMove(float dt)
-    {
-        var ballPosition = new Vector2(transform.position.x, transform.position.y);
-		var dis = Vector2.SqrMagnitude(ballPosition - nextWayPoint);
-        Vector2 wayPoint;
-        if (dis <= 0.01f)
-        {
-            if (wayPoints.TryDequeue(out wayPoint))
-            {
-                nextWayPoint = wayPoint;
-                Debug.Log(nextWayPoint);
-            }
-        }
+		if (speed > 0)
+		{
+			speed -= Time.deltaTime * 10;
+			if (speed < 0)
+			{
+				speed = 0;
+			}
+		}
+	}
 
-		dir = (nextWayPoint - ballPosition).normalized;
-		rb.velocity = speed * dt * dir;
-    }
+	private void FreeMove(float dt)
+	{
+		if (wayPoints.Count > 0)
+		{
+			var ballPosition = new Vector2(transform.position.x, transform.position.y);
+			var dis = Vector2.SqrMagnitude(ballPosition - nextWayPoint);
+			if (dis <= 1f)
+			{
+				if (wayPoints.TryDequeue(out var wayPoint))
+				{
+					nextWayPoint = wayPoint;
+					dir = (nextWayPoint - ballPosition).normalized;
+				}
+			}
+		}
 
-    void StateFree(float dt)
-    {
-        int num = ballCollider.OverlapCollider(contactFilter, others);
-        if (num > 0)
-        {
-            for (int i = 0; i < num; i++)
-            {
-                var player = others[i].GetComponentInParent<IPlayer>();
-                if (player is not null)
-                {
-                    player.OnCatchBall(gameObject);
-                    Owner = others[i].gameObject;
-                    state = BallState.Held;
-                    continue;
-                }
-            }
-        }
-        else
-        {
-            FreeMove(dt);
-        }
-    }
+		transform.position += dir * speed * dt;
+	}
 
-    void StateHeld(float dt)
-    {
-        transform.position = Owner.transform.position;
-    }
+	void StateFree(float dt)
+	{
+		int num = ballCollider.OverlapCollider(contactFilter, others);
+		if (num > 0)
+		{
+			for (int i = 0; i < num; i++)
+			{
+				var player = others[i].GetComponentInParent<IPlayer>();
+				if (player is not null)
+				{
+					player.OnCatchBall(gameObject);
+					Owner = others[i].gameObject;
+					state = BallState.Held;
+					continue;
+				}
+			}
+		}
+		else
+		{
+			FreeMove(dt);
+		}
+	}
 
-    public void SetOwner(GameObject owner)
-    {
-        Owner = owner;
-        state = BallState.Held;
-    }
+	void StateHeld(float dt)
+	{
+		transform.position = Owner.transform.position;
+	}
 
-    #region IBall
-    public GameObject GetOwner()
-    {
-        return Owner;
-    }
+	public void SetOwner(GameObject owner)
+	{
+		Owner = owner;
+		state = BallState.Held;
+	}
 
-    public void Shoot()
-    {
-        Debug.Log(string.Join(" ", wayPoints.Take(5)));
-        Owner = null;
-        nextWayPoint = wayPoints.Skip(1).First();
-        wayPoints.Dequeue();
-        state = BallState.Free;
-        StartCoroutine(resetContact());
-    }
+	#region IBall
+	public GameObject GetOwner()
+	{
+		return Owner;
+	}
 
-    IEnumerator resetContact()
-    {
-        var filter = contactFilter;
-        var mask = contactFilter.layerMask;
-        mask = LayerMask.NameToLayer("None");
-        filter.layerMask = mask;
-        // todo: fix time
-        yield return new WaitForSeconds(noContactTime);
-        contactFilter = filter;
-    }
+	public void Shoot()
+	{
+		Debug.Log(string.Join(" ", wayPoints.Take(5)));
+		Owner = null;
+		nextWayPoint = wayPoints.Skip(1).First();
+		wayPoints.Dequeue();
+		state = BallState.Free;
+		StartCoroutine(resetContact());
+	}
 
-    public void SetPath(List<Vector2> pathPoints)
-    {
-        wayPoints = new Queue<Vector2>(pathPoints);
-    }
+	IEnumerator resetContact()
+	{
+		var filter = contactFilter;
+		var mask = contactFilter.layerMask;
+		mask = LayerMask.NameToLayer("None");
+		filter.layerMask = mask;
+		// todo: fix time
+		yield return new WaitForSeconds(noContactTime);
+		contactFilter = filter;
+	}
 
-    public float SetInitSpeed(float speed)
-    {
-        this.speed = speed;
-        return speed;
-    }
+	public void SetPath(List<Vector2> pathPoints)
+	{
+		wayPoints = new Queue<Vector2>(pathPoints);
+	}
 
-    public void DoMove(float deltaTime)
-    {
-        switch (state)
-        {
-            case BallState.Free:
-                StateFree(deltaTime);
-                break;
-            case BallState.Held:
-                StateHeld(deltaTime);
-                break;
-            default:
-                break;
-        }
-    }
+	public float SetInitSpeed(float speed)
+	{
+		this.speed = speed;
+		return speed;
+	}
 
-    public bool CheckIsScore()
-    {
-        return isScore;
-    }
+	public void DoMove(float deltaTime)
+	{
+		switch (state)
+		{
+			case BallState.Free:
+				StateFree(deltaTime);
+				break;
+			case BallState.Held:
+				StateHeld(deltaTime);
+				break;
+			default:
+				break;
+		}
+	}
 
-    public bool CheckOutLine()
-    {
-        return isOutLine;
-    }
+	public bool CheckIsScore()
+	{
+		return isScore;
+	}
 
-    public bool CheckCatched()
-    {
-        return ownerPlayer != null;
-    }
+	public bool CheckOutLine()
+	{
+		return isOutLine;
+	}
 
-    public void SetOutLine(bool value)
-    {
-        isOutLine = value;
-    }
+	public bool CheckCatched()
+	{
+		return ownerPlayer != null;
+	}
 
-    public void SetIsScore(bool value)
-    {
-        isScore = value;
-    }
+	public void SetOutLine(bool value)
+	{
+		isOutLine = value;
+	}
 
-    bool isOutLine;
-    bool isScore;
-    #endregion IBall
+	public void SetIsScore(bool value)
+	{
+		isScore = value;
+	}
+
+	bool isOutLine;
+	bool isScore;
+	#endregion IBall
 }
